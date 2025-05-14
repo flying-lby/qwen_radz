51d91df0b828c3034de35a4223329a070a8bd870
flying-lby <2230232178@qq.com>
2025-05-15 03:16:05 +0800

    11

--- Qwen2.5-VL/qwen-vl-finetune/scripts/A100/qwen_radz_wosft_v3/eval_chestxray_classify.sh
+++ Qwen2.5-VL/qwen-vl-finetune/scripts/A100/qwen_radz_wosft_v3/eval_chestxray_classify.sh
@@ -10,6 +10,7 @@
 # ========================
 echo "Starting evaluation process..."
 
+
 python -m eval.run_eval \
     --model-path /mnt/nlp-ali/usr/huangwenxuan/home/qwen_vl_7b/Qwen2.5-VL-7B-Instruct \
     --answer_file ./results/Qwen2.5-VL-7B-Instruct/siim/answers.jsonl \
