a2588b2a6be81f8ac1b33a2f8bd808107391709f
flying-lby <2230232178@qq.com>
2025-09-10 17:42:38 +0800

    修复eval_utils模块导入错误：移除不存在的dataset_rsna和grounding_metrics模块引用

--- qwen_finetune/Qwen2-VL-Finetune/src/eval_utils/__init__.py
+++ qwen_finetune/Qwen2-VL-Finetune/src/eval_utils/__init__.py
@@ -4,14 +4,14 @@ from .dataset import ClipClassificationDataset
 from .progress_tracker import ProgressTracker
 from .utils import load_image_file, split_list, get_chunk
 
-# Grounding评估模块
-from .dataset_rsna import RSNAGroundingDataset, create_rsna_dataloader
-from .grounding_metrics import (
-    calculate_grounding_scores, 
-    aggregate_grounding_results,
-    visualize_attention_map,
-    extract_attention_from_model_output
-)
+# Grounding评估模块 - 暂时注释掉不存在的模块
+# from .dataset_rsna import RSNAGroundingDataset, create_rsna_dataloader
+# from .grounding_metrics import (
+#     calculate_grounding_scores, 
+#     aggregate_grounding_results,
+#     visualize_attention_map,
+#     extract_attention_from_model_output
+# )
 
 # 未使用的模块 (可考虑移除或保留作为扩展)
 # from .metrics import MetricCalculator, calculate_aucs, calculate_f1_scores, calculate_accuracy
@@ -24,11 +24,11 @@ __all__ = [
     'load_image_file',
     'split_list',
     'get_chunk',
-    # Grounding模块
-    'RSNAGroundingDataset',
-    'create_rsna_dataloader',
-    'calculate_grounding_scores',
-    'aggregate_grounding_results',
-    'visualize_attention_map', 
-    'extract_attention_from_model_output'
+    # Grounding模块 - 暂时移除不存在的模块
+    # 'RSNAGroundingDataset',
+    # 'create_rsna_dataloader',
+    # 'calculate_grounding_scores',
+    # 'aggregate_grounding_results',
+    # 'visualize_attention_map', 
+    # 'extract_attention_from_model_output'
 ]
